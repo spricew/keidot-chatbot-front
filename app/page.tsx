3d68bf97ea41c349abd705a13cedab091af3f9a1
Heyder Medina <medinaheyder@gmail.com>
2026-06-17 16:58:59 -0600

    feat: initial page structure added

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="">
-      
+    <div className="flex w-full h-screen justify-center items-center bg-black">
+      <div className="w-1/2 h-full flex flex-col justify-center p-4">
+        <h1 className="text-white text-4xl tracking-tight">Bienvenido de vuelta</h1>
+        <h1 className="text-white text-2xl">¿Como puedo ayudarte hoy?</h1>
+        
+        <input type="text" placeholder="Buscar"
+         className="flex items-center justify-center w-full h-16 rounded-full bg-surface-container ring ring-white/20" />
+      </div>
+
     </div>
   );
 }
