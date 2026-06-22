78c9c18b16899f5b4db2a82ba1a8f6aa5e344dfc
Heyder Medina <medinaheyder@gmail.com>
2026-06-22 00:02:57 -0600

    feat: add button with IconArrowUp for user input submission

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Aurora from '@/app/components/ui/animated/Aurora';
 import DarkVeil from '@/app/components/ui/animated/DarkVeil';
 
+import { IconArrowUp } from '@tabler/icons-react';
+
 export default function Home() {
   return (
     <div className="flex w-full h-screen justify-center items-center bg-black">
@@ -30,12 +32,16 @@ export default function Home() {
           <h2 className="text-white text-xl">¿Como puedo ayudarte hoy?</h2>
         </div>
 
-        <div className="h-full flex">
+        <div className="flex items-center h-16 gap-x-1">
           <input type="text" placeholder="Preguntarle a Keidot"
-            className="flex items-center justify-center w-full h-14 py-2 px-6 rounded-full 
+            className="flex items-center justify-center w-full h-full py-2 px-6 rounded-full 
            bg-white/10 ring ring-inset ring-white/10 self-end
             backdrop-blur-3xl"
           />
+
+          <button className='flex items-center justify-center bg-black/10 backdrop-blur-2xl ring ring-inset ring-white/10 rounded-full h-full aspect-square '>
+            <IconArrowUp stroke={2} />
+          </button>
         </div>
 
       </div>
