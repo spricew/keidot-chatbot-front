07c3c2ae981681d339e2ee4f2acc4368d416e22c
Heyder Medina <medinaheyder@gmail.com>
2026-06-21 23:44:49 -0600

    feat: input style updated to glassmorphism style

--- app/page.tsx
+++ app/page.tsx
@@ -23,22 +23,18 @@ export default function Home() {
         />
       </div>
 
-      <div className="w-1/2 h-full grid grid-rows-[3fr_1fr] z-10">
+      <div className="w-1/2 h-full grid grid-rows-[6fr_1fr] py-10 z-10">
 
         <div className="flex flex-col gap-0.5 justify-center">
           <h1 className="text-white text-4xl tracking-tight">Bienvenido de vuelta</h1>
           <h2 className="text-white text-xl">¿Como puedo ayudarte hoy?</h2>
         </div>
 
-        <div className="h-full flex pb-12">
-          {/* <input type="text" placeholder="Preguntarle a Keidot"
-            className="flex items-center justify-center w-full h-14 py-2 px-6 rounded-full 
-          bg-surface-container ring ring-white/20 self-end"
-          /> */}
+        <div className="h-full flex">
           <input type="text" placeholder="Preguntarle a Keidot"
             className="flex items-center justify-center w-full h-14 py-2 px-6 rounded-full 
-          bg-white/10 ring ring-inset ring-white/10 self-end
-          backdrop-blur-2xl"
+           bg-white/10 ring ring-inset ring-white/10 self-end
+            backdrop-blur-3xl"
           />
         </div>
 
